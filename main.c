5e598b519f38d364132b57bceac1d99be3bc2301
andbasso <andrebasso92@hotmail.com>
2013-07-10 22:32:17 -0300

    Update main.c
    atualizando o main.c para funcionar com a nova mutex.h

--- main.c
+++ main.c
@@ -1,55 +1,80 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include <unistd.h>
 #include "mutex.h"
-#define MAX 10
 
-pthread_mutex_t mutex;
-pthread_cond_t condConsumidor;
-pthread_cond_t condProdutor;
-int buffer = 0;
+#define N_ITENS 10
+int buffer[N_ITENS];
 
-void *produtor() {
+int mutex = 0, consumido = 0, produzido = 0, cont = 0, inicio = 0, final = 0;
+
+void* produtor(void *v) {
   int i;
-	
-	for(i = 0; i <= MAX; i++){
-		lock(&mutex)
-		while( buffer != 0) {
-			wait(&condProdutor)
-		}	
-		buffer = i;
-		signal(&condProdutor);
-		unlock(&mutex);
-	}
-	pthread_exit(0);
+  
+  for (i = 0; i < 3 * N_ITENS; i++) {
+	  while (cont == 10) ;
+      if(mutex == 0) {
+	    //mutex++;
+		lock(mutex);
+        printf("Produtor, item = %d, mutex = %d", i, mutex);     
+        final = (final + 1) % N_ITENS;
+		if(buffer[final] != 0)
+			printf("posicao ocupada ERROR");
+        buffer[final] = 1;
+		produzido++;
+		cont++;
+        //mutex--;
+		unlock(mutex);
+		printf(" mutexF = %d\n", mutex);     
+      }
+	sleep(random() % 5);
+  }
+  
+  return NULL;
 }
 
-void *consumidor() {
-	int i;
-	
-	for(i = 0; i <= MAX; i++){
-		lock(&mutex);
-		while(buffer == 0) {
-			wait(&condConsumidor, &mutex);
-		}
-		buffer = 0;
-		signal(&condConsumidor);
-		unlock(&mutex);
-	}
-	pthread_exit(0);
+void* consumidor(void *v) {
+  int i, consumir;
+
+  for (i = 0; i < 3 * N_ITENS; i++) {
+	while (cont == 0);
+    if(mutex == 0){
+        //mutex++;
+        lock(mutex);
+        inicio = (inicio + 1) % N_ITENS;
+        consumir = buffer[inicio];
+		if(buffer[inicio] != 1)
+			printf("posicao sem nada ERROR");
+		buffer[inicio] = 0;
+		consumido++;
+        printf("Consumidor, item = %d, mutex = %d", i, mutex);     ;
+        if (consumido > produzido)
+            printf("Erro: item foi consumido antes de ser produzido.\n");
+		cont--;
+        unlock(mutex);
+		printf(" mutexF = %d\n", mutex);     
+    }
+	sleep(random() % 5);
+  }
+  return NULL;
 }
 
 int main() {
-	pthread_t produtor, consumidor;
-	pthread_mutex_init(&mutex, 0);
-	pthread_cond_init(&condProdutor, 0);
-	pthread_cond_init(&condConsumidor, 0);
-	pthread_create(&condProdutor, 0, produtor);
-	pthread_create(&condConsumidor, 0, consumidor);
-	pthread_join(produtor, 0);
-	pthread_join(consumidor, 0);
-	pthread_cond_destroy(&condProdutor, 0);
-	pthread_cond_destroy(&condConsumidor, 0);
-	pthread_mutex_destroy(&mutex, 0);
+  int i;
+  
+  pthread_t thr_produtor, thr_consumidor;
+
+  /* Limpa o buffer */
+  for (i = 0; i < N_ITENS; i++)
+    buffer[i] = 0;
+  
+  pthread_create(&thr_produtor, NULL, produtor, NULL);
+  pthread_create(&thr_consumidor, NULL, consumidor, NULL);
+
+  pthread_join(thr_produtor, NULL); 
+  pthread_join(thr_consumidor, NULL);
+
+  return 0;
 }
 
